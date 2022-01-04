49965ed247c787280153ec3a711e2667944c80ee
gibbsnich <gibbsnich@gmail.com>
2022-01-04 20:49:31 +0100

    remove quasar boilerplate

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,26 +1,8 @@
-
 const routes = [
-
   { path: '/', component: () => import('pages/Home.vue') },
   { path: '/recipes', component: () => import('pages/Recipes.vue') },
   { path: '/ingredients', component: () => import('pages/Ingredients.vue') },
   { path: '/categories', component: () => import('pages/Categories.vue') },
-/*
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
-  },
-*/
-  // Always leave this as last one,
-  // but you can also remove it
-  /*
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
-  }*/
 ]
 
 export default routes
